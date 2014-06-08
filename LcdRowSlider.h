3d2cb4da7270e7a48e2de68060e9a422751191c2
Andrew Duncan <arduncan@bigpond.com>
2014-06-08 22:08:17 +1000

    Now templated so that any LCD library can be used

--- LcdRowSlider.h
+++ LcdRowSlider.h
@@ -2,7 +2,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2013 Andrew Duncan
+// Copyright (c) 2014 Andrew Duncan
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the
@@ -38,40 +38,87 @@
 #include "WProgram.h"
 #endif
 
-#include "LiquidCrystal.h"
-
 //-------------------------------------------------------------------------
 
+template<typename LiquidCrystal_T>
 class LcdRowSlider 
 {
 public:
 
     LcdRowSlider(
-        LiquidCrystal& lcd,
+        LiquidCrystal_T& lcd,
         int32_t min,
         int32_t max,
         uint8_t row,
         uint8_t firstColumn = 0,
-        uint8_t lastColumn = 15);
+        uint8_t lastColumn = 15)
+    :
+        _lcd(lcd),
+        _min(min),
+        _max(max),
+        _row(row),
+        _firstColumn(firstColumn),
+        _columns(lastColumn - firstColumn + 1),
+        _setup(false)
+    {
+    }
+
+    //---------------------------------------------------------------------
 
     void value(int32_t value)
     {
         unscaled(map(value, _min, _max, 0, (_columns * 5) - 1));
     }
 
-    void unscaled(uint8_t value);
+    void unscaled(uint8_t value)
+    {
+        if (not _setup)
+        {
+            setup();
+        }
+
+        uint8_t letters = value / 5;
+        uint8_t bar = value % 5;
+        uint8_t letter = 0;
+
+        _lcd.setCursor(letter + _firstColumn, _row);
+
+        for ( ; letter < letters ; ++letter)
+        {
+            _lcd.write((uint8_t)5);
+        }
+
+        ++letter;
+        _lcd.write(bar);
+
+        for ( ; letter < _columns ; ++letter)
+        {
+            _lcd.write((uint8_t)5);
+        }
+    }
+
+    //---------------------------------------------------------------------
 
     uint8_t getRow() const { return _row; }
+
+    //---------------------------------------------------------------------
 
     int32_t getMin() const { return _min; }
     int32_t getMax() const { return _max; }
 
-    void setColumns(uint8_t first, uint8_t last);
+    //---------------------------------------------------------------------
+
+    void setColumns(uint8_t first, uint8_t last)
+    {
+        _firstColumn = first;
+        _columns = last - first + 1;
+    }
+
     void setMinMax(int32_t min, int32_t max) { _min = min; _max = max; }
 
 private: 
 
-    LiquidCrystal& _lcd;
+    LiquidCrystal_T& _lcd;
     int32_t _min;
     int32_t _max;
     uint8_t _row;
@@ -79,9 +126,22 @@ private:
     uint8_t _columns;
     boolean _setup;
 
-    void setup();
+    //---------------------------------------------------------------------
+
+    void setup()
+    {
+        for (uint8_t letter = 0 ; letter <= 5 ; ++letter)
+        {
+            uint8_t pixel = (letter == 5) ? 0 : 1 << (4 - letter);
+            uint8_t pixels[8] = { 0, pixel, pixel, 0x1F, pixel, pixel, 0, 0 };
+            _lcd.createChar(letter, pixels);
+        }
+
+        _setup = true;
+    }
 };
 
 //-------------------------------------------------------------------------
 
 #endif
+
